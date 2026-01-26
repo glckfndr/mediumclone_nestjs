4cd163ce405578e1e86d779892dcde8985954cd1
glckfndr <obulan65@gmail.com>
2026-01-26 18:32:23 +0200

    feat: add createArticle in db

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from '@app/article/article.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { CreateArticleDto } from './dto/createArticleDto';
+import { User } from '@app/decorators/user.decorator';
+import { UserEntity } from '@app/user/user.entity';
+import { ArticleEntity } from './article.entity';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
-  createArticle(): any {
-    return this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async createArticle(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    return await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
   }
 
   @Get()
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/createArticleDto';
+import { ArticleEntity } from './article.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '@app/user/user.entity';
 
 @Injectable()
 export class ArticleService {
-  createArticle() {
-    return 'Create Article';
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  createArticle(
+    currentUser: UserEntity,
+    articleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, articleDto);
+    article.author = currentUser;
+    if (article.tagList == null) {
+      article.tagList = [];
+    }
+    article.slug = 'foo slug'; // TODO: generate slug
+    return this.articleRepository.save(article);
   }
+
   findAllArticles() {
     return 'Find All Articles';
   }
