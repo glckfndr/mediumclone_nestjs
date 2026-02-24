01675c5e6e40a68bbf06bad3ee82dc55ecc1d702
glckfndr <obulan65@gmail.com>
2026-02-24 20:05:00 +0200

    feat: add many to many relations between article and user

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -5,6 +5,8 @@ import {
   BeforeInsert,
   Column,
   Entity,
+  JoinTable,
+  ManyToMany,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
@@ -36,4 +38,7 @@ export class UserEntity {
 
   @OneToMany(() => ArticleEntity, (article) => article.author)
   articles: ArticleEntity[];
+  @ManyToMany(() => ArticleEntity)
+  @JoinTable()
+  favorites: ArticleEntity[];
 }
